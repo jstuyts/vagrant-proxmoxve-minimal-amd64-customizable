4b609e1d686b00623d5b24fc2f9b2fcdef1e68f7
Johan Stuyts <j.stuyts@javathinker.com>
2016-01-15 18:04:50 +0100

    Added script to clean the build artifacts: custom ISO, "build" directory and the boxes.

---
+++ Clean.ps1
@@ -0,0 +1,3 @@
+﻿Remove-Item *.box -Force -ErrorAction SilentlyContinue
+Remove-Item iso\custom.iso -Force -ErrorAction SilentlyContinue
+Remove-Item build -Recurse -Force -ErrorAction SilentlyContinue
